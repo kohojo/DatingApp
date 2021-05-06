1d213d0add54795d0a2fe5b1020d0a4923870467
Josef Kohoutek <josef.kohoutek@technodat.eu>
2021-05-06 13:31:15 +0200

    End of section 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,8 +1,13 @@
 using System; 
 using System.Collections.Generic; 
-using System.Linq; 
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks; 
-using API.Data; 
+using API.Data;
+using API.Extensions;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder; 
 using Microsoft.AspNetCore.Hosting; 
 using Microsoft.AspNetCore.HttpsPolicy; 
@@ -11,7 +16,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration; 
 using Microsoft.Extensions.DependencyInjection; 
 using Microsoft.Extensions.Hosting; 
-using Microsoft.Extensions.Logging; 
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models; 
 
 namespace API {
@@ -25,15 +31,15 @@ namespace API {
      
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services) {
-            services.AddDbContext < DataContext > (options =>  {
-                options.UseSqlite(_config.GetConnectionString("DefaultConnection")); 
-            }); 
+        public void ConfigureServices(IServiceCollection services) 
+        {
+            services.AddApplicationServices(_config);
             services.AddControllers(); 
             services.AddCors();
             services.AddSwaggerGen(c =>  {
                 c.SwaggerDoc("v1", new OpenApiInfo {Title = "API", Version = "v1"}); 
             }); 
+            services.AddIdentityServices(_config);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +57,8 @@ namespace API {
             //add because we call API (https://localhost:5001) from another origin (http://localhost:4200, https://localhost:4200)
             app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200")); 
 
+            app.UseAuthentication();
+            
             app.UseAuthorization(); 
 
             app.UseEndpoints(endpoints =>  {
