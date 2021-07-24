4c247e840bf9c464a075d5e524e7966ca388e7ad
Josef Kohoutek <josef.kohoutek@technodat.eu>
2021-07-24 11:21:24 +0200

    End of lesson 59

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -13,34 +13,19 @@ export class AppComponent implements OnInit {
   users: any;
 
   constructor(
-    private http: HttpClient,
     private accountService: AccountService
   ) {}
 
-  ngOnInit() {
-    this.getUsers();
-    this.setCurrentUser();
-  }
 
-  // setCurrentUser(){
-  //   const user: User = JSON.parse(localStorage.getItem('user'));
-  // }
+  ngOnInit(): void {
+  }
 
   setCurrentUser() {
     const user: User = JSON.parse(localStorage.getItem('user') ?? '{}');
     this.accountService.setCurrentUser(user);
   }
 
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe(
-      (response) => {
-        this.users = response;
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
-  }
+
 }
 
 
